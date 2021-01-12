74593c334737cabc0a1faced74f8cd1815c8f8d3
tristanhowells <76887867+tristanhowells@users.noreply.github.com>
2021-01-12 17:09:32 +1100

    Update script.py

--- script.py
+++ script.py
@@ -400,18 +400,14 @@ class DQNAgent:
             return
 
         # Get a minibatch of random samples from memory replay table
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        if np.array([transition[0] for transition in minibatch]).shape == (32, 30, 5):
+            current_states = np.array([np.array(transition[0]) for transition in minibatch])
         else:
-            minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
-
-            if np.array([transition[0] for transition in minibatch]).shape == (32, 30, 5):
-                current_states = np.array([transition[0] for transition in minibatch])
-            else:
-                return
-#             while np.array([transition[0] for transition in minibatch]).shape != (32, 30, 5):
-#                 minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
-#                 current_states = np.array([transition[0] for transition in minibatch])      
-       
-
+            while np.array([transition[0] for transition in minibatch]).shape != (32, 30, 5):
+                minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+                current_states = np.array(np.array([transition[0]) for transition in minibatch])      
 
         # current_states = tf.convert_to_tensor(current_states, dtype=tf.float32)
         # print("current_states type: ", type(current_states))
