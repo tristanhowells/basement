7729d74fd42afa51556749fb8b2a1393e30cdb5f
tristanhowells <76887867+tristanhowells@users.noreply.github.com>
2021-01-27 21:37:09 +1100

    Update tf_script_v2.py

--- tf_script_v2.py
+++ tf_script_v2.py
@@ -27,7 +27,7 @@ print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('
 print("Start Date: ", today) 
 
 ###LOAD EXISTING MODEL
-LOAD_MODEL = r'/storage/test/' #filepath or none
+LOAD_MODEL = r'/storage/test/.' #filepath or none
 
 if LOAD_MODEL is not None:
     print(f'Loading {LOAD_MODEL}')
