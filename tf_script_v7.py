75b1e46c5724dbc912bc0c803d412d333792c6b6
tristanhowells <76887867+tristanhowells@users.noreply.github.com>
2021-05-10 21:01:54 +1000

    Update tf_script_v7.py

--- tf_script_v7.py
+++ tf_script_v7.py
@@ -69,7 +69,7 @@ AGGREGATE_STATS_EVERY = 50  # episodes
 
 ### build Episodes
 ### 500, 1000, 2500, 5000, 10000  
-DATA_SAMPLES = 1001
+DATA_SAMPLES = 10001
 
 def find_csv_filenames( path_to_dir, suffix=".csv" ):
     filenames = listdir(path_to_dir)
