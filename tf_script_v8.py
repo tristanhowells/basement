e4959804e0dd541c2a46f9b841f248a67f180927
tristanhowells <76887867+tristanhowells@users.noreply.github.com>
2021-05-22 12:33:52 +1000

    Update tf_script_v8.py

--- tf_script_v8.py
+++ tf_script_v8.py
@@ -80,9 +80,8 @@ def detrend(data):
     data = pd.DataFrame(data)
 #     DATA = data.pct_change()
 #     DATA = DATA.iloc[1:]
-#     DATA.replace([np.inf, np.nan], 0, inplace=True)
-#     DATA = pd.DataFrame.to_numpy(DATA)
-    DATA = data
+    DATA.replace([np.inf, np.nan], 0, inplace=True)
+    DATA = pd.DataFrame.to_numpy(DATA)
     return DATA
 
 def episode_window(data):
