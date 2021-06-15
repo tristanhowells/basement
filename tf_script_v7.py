a0792800b0fd2e774fe71a6f5cb049a2fbab8153
tristanhowells <76887867+tristanhowells@users.noreply.github.com>
2021-06-15 22:03:47 +1000

    Update tf_script_v7.py

--- tf_script_v7.py
+++ tf_script_v7.py
@@ -33,7 +33,7 @@ print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('
 print("Start Date: ", today) 
 
 ###LOAD EXISTING MODEL
-LOAD_MODEL = r'/storage/models/latest_May-29-2021/_0' 
+LOAD_MODEL = None #r'/storage/models/latest_May-29-2021/_0' 
 
 if LOAD_MODEL is not None:
     print(f'Loading {LOAD_MODEL}')
@@ -69,7 +69,7 @@ AGGREGATE_STATS_EVERY = 50  # episodes
 
 ### build Episodes
 ### 500, 1000, 2500, 5000, 10000  
-DATA_SAMPLES = 10001
+DATA_SAMPLES = 7501
 
 def find_csv_filenames( path_to_dir, suffix=".csv" ):
     filenames = listdir(path_to_dir)
